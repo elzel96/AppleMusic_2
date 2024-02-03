f0c59dbb8d962f1c7d1a1549c33fc002d5814c35
elzel <elzel666@gmail.com>
2024-02-03 16:52:02 +0300

    Initial Commit

---
+++ AppleMusic_2/AppleMusic_2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_2App.swift
+//  AppleMusic_2
+//
+//  Created by Helena on 3.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
